a6f255c90d86683d7a571ec6a114ab69b7af7146
Alex Huatay Casas <85271363+IDozy@users.noreply.github.com>
2026-02-18 16:32:35 +0000

    agregando rutin de prisma

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    // Solo aplicar en el servidor (donde se usa Prisma)
+    if (isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@prisma/client-runtime-utils': path.join(
+          __dirname,
+          'node_modules/@prisma/client-runtime-utils'
+        ),
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
